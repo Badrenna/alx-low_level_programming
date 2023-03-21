dc174d44959ade568a32a04fef3148cac9fc8dc2
badrenna <badr.ennadif@gmail.com>
2023-03-21 10:25:16 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alphabet
+ *
+ * Return: void
+ */
+
+int _islower(int c)
+{
+
+      	if (c>=65 && 90>=c) {
+        return 0;
+   	 } else {
+        return 1;
+   	 }
+}
